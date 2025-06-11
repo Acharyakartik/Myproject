0719e60cae7450e0b9a79dbbe9622e49eb78688c
Acharyakartik <kgacharya08@gmail.com>
2025-06-11 19:13:38 +0530

    this is the issue that the slove the python program

--- train mangement project/train_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Train
-
-@admin.register(Train)
-class TrainAdmin(admin.ModelAdmin):
-    list_display = ('name', 'number', 'source', 'destination', 'departure_time', 'arrival_time', 'seats_available', 'ticket_price')
-    search_fields = ('name', 'number', 'source', 'destination')
-    list_filter = ('source', 'destination')
--- train mangement project/train_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.train_list, name='train_list'),
-    path('add/', views.add_train, name='add_train'),
-]
